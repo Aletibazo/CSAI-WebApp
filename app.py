84da22f40370584d692962f436d768ebb7abcc4f
Aletibazo <aletibazo@gmail.com>
2019-07-05 18:50:43 +0200

    Versión estable, ejecutable por servicio"

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2.7
+
 from flask import Flask, render_template, json, request, session, redirect
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
